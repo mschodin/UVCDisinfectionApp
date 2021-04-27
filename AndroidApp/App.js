c60de9aedf42c482159b9ec30a950085142807ea
mschodin <michael-schodin@uiowa.edu>
2021-04-27 11:13:22 -0500

    Cleaned up unused code

--- AndroidApp/App.js
+++ AndroidApp/App.js
@@ -1,20 +1,9 @@
-import React, { useState } from "react";
-import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import React from "react";
 import Control from "./Control";
-import Connect from "./Connect";
-// import { BleManager } from 'react-native-ble-plx';
 
 export default function App() {
-    // const [v, setVar] = useState("00:00");
-    // const [count, setCount] = useState(0);
-    // function handlePress() {
-        // setCount(5);
-    // }
-
-
     return (
         <Control/>
-        // <Connect/>
     );
 }
 
